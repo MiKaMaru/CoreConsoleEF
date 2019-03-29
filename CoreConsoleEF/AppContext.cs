c47c4d89912cd6f4c14b6abca0c9076cbc62f81f
Karachev <Karachev.MA@parmalogica.ru>
2019-03-29 15:07:31 +0500

    Добавьте файлы проекта.

---
+++ CoreConsoleEF/AppContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoreConsoleEF
+{
+    ///первый способ подключения без поддержки конфигурационного json файла
+    ////DbContext - определяет контекст данных
+    //public class AppContext : DbContext
+    //{
+    //    private static string dbName = "P10131\\SQLEXPRESS";
+    //    private string strSqlConn = "Server="+dbName+";Database=hello1;Trusted_Connection=True;";
+    //    //DbSet - предоставляет набор объектов, хранящихся в БД
+    //    //virtual - если уже создано и есть внутренние правила
+    //    public DbSet<User> Users { get; set; }
+    //    public AppContext()
+    //    {
+    //        //если нет БД, то создаст
+    //        Database.EnsureCreated();
+    //    }
+    //    //самая длинная фигня в функции - устанавливает параметры подключения / билдер подключения
+    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //    {
+    //        optionsBuilder.UseSqlServer(strSqlConn);
+    //    }
+    //}
+
+        /// второй способ поключения
+    public class AppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppContext(DbContextOptions<AppContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ CoreConsoleEF/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+
+namespace CoreConsoleEF
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Добавление
+            using (AppContext db = new AppContext())
+            {
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Alice", Age = 26 };
+
+                // Добавление
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+
+                // analog:
+                /*
+                 db.Users.AddRange(user1, user2);
+                db.SaveChanges();
+                 */
+            }
+
+            // получение/Чтение
+            using (AppContext db = new AppContext())
+            {
+                // получаем объекты из бд и выводим на консоль
+                var users = db.Users.ToList();
+                Console.WriteLine("Данные после добавления:");
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+
+            // Редактирование
+            using (AppContext db = new AppContext())
+            {
+                // получаем первый объект
+                User user = db.Users.FirstOrDefault();
+                if (user != null)
+                {
+                    user.Name = "Bob";
+                    user.Age = 44;
+                    //обновляем объект. нужно только в том случае, если мы внесли изменения вне конструкции using
+                    //db.Users.Update(user);
+                    //db.Users.UpdateRange(user1, user2);
+                    db.SaveChanges();
+                }
+                // выводим данные после обновления
+                Console.WriteLine("\nДанные после редактирования:");
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+
+            // Удаление
+            using (AppContext db = new AppContext())
+            {
+                // получаем первый объект
+                User user = db.Users.FirstOrDefault();
+                if (user != null)
+                {
+                    //удаляем объект
+                    db.Users.Remove(user);
+                    db.SaveChanges();
+                    // analog:
+                    /*
+                     db.Users.RemoveRange(user1, user2);
+                db.SaveChanges();
+                     */
+                }
+                // выводим данные после обновления
+                Console.WriteLine("\nДанные после удаления:");
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+            Console.Read();
+        }
+    }
+}
