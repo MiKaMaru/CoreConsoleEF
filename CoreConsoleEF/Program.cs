c6a3d628443e10b62a54b7c087697e26dc1a162e
MiKa <obito_uchiha@inbox.ru>
2019-04-17 15:00:20 +0500

    ManyToMany - Полностью реализовано и протестировано. P.S. Функционала создания данного типа связи без промежуточного класс из EF 6 в EF Core ещё не завезли

--- CoreConsoleEF/Program.cs
+++ CoreConsoleEF/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Json;
@@ -9,6 +10,7 @@ namespace CoreConsoleEF
 {
     public class Program
     {
+        
         public static void Main(string[] args)
         {
 
@@ -33,12 +35,18 @@ namespace CoreConsoleEF
             {
                 User user1 = new User { Name = "Tom", Age = 33 };
                 User user2 = new User { Name = "Alice", Age = 26 };
+                Home home = new Home { Name = "Petropav"};
 
+                HomeUser homeUser = new HomeUser{Home = home, User = user1};
+                
                 // Добавление
                 db.Users.Add(user1);
                 db.Users.Add(user2);
-                db.SaveChanges();
 
+                db.Homes.Add(home);
+
+                db.HomeUsers.Add(homeUser);
+                db.SaveChanges();
                 // analog:
                 /*
                  db.Users.AddRange(user1, user2);
@@ -51,60 +59,65 @@ namespace CoreConsoleEF
             {
                 // получаем объекты из бд и выводим на консоль
                 var users = db.Users.ToList();
+                var homes = db.Homes.ToList();
                 Console.WriteLine("Данные после добавления:");
                 foreach (User u in users)
                 {
                     Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
                 }
-            }
-
-            // Редактирование
-            using (AppContext db = new AppContext(options))
-            {
-                // получаем первый объект
-                User user = db.Users.FirstOrDefault();
-                if (user != null)
+                foreach (Home u in homes)
                 {
-                    user.Name = "Bob";
-                    user.Age = 44;
-                    //обновляем объект. нужно только в том случае, если мы внесли изменения вне конструкции using
-                    //db.Users.Update(user);
-                    //db.Users.UpdateRange(user1, user2);
-                    db.SaveChanges();
-                }
-                // выводим данные после обновления
-                Console.WriteLine("\nДанные после редактирования:");
-                var users = db.Users.ToList();
-                foreach (User u in users)
-                {
-                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                    Console.WriteLine($"{u.Id}.{u.Name}");
                 }
             }
 
-            // Удаление
-            using (AppContext db = new AppContext(options))
-            {
-                // получаем первый объект
-                User user = db.Users.FirstOrDefault();
-                if (user != null)
-                {
-                    //удаляем объект
-                    db.Users.Remove(user);
-                    db.SaveChanges();
-                    // analog:
-                    /*
-                     db.Users.RemoveRange(user1, user2);
-                db.SaveChanges();
-                     */
-                }
-                // выводим данные после обновления
-                Console.WriteLine("\nДанные после удаления:");
-                var users = db.Users.ToList();
-                foreach (User u in users)
-                {
-                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
-                }
-            }
+            //// Редактирование
+            //using (AppContext db = new AppContext(options))
+            //{
+            //    // получаем первый объект
+            //    User user = db.Users.FirstOrDefault();
+            //    if (user != null)
+            //    {
+            //        user.Name = "Bob";
+            //        user.Age = 44;
+            //        //обновляем объект. нужно только в том случае, если мы внесли изменения вне конструкции using
+            //        //db.Users.Update(user);
+            //        //db.Users.UpdateRange(user1, user2);
+            //        db.SaveChanges();
+            //    }
+            //    // выводим данные после обновления
+            //    Console.WriteLine("\nДанные после редактирования:");
+            //    var users = db.Users.ToList();
+            //    foreach (User u in users)
+            //    {
+            //        Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+            //    }
+            //}
+
+            //// Удаление
+            //using (AppContext db = new AppContext(options))
+            //{
+            //    // получаем первый объект
+            //    User user = db.Users.FirstOrDefault();
+            //    if (user != null)
+            //    {
+            //        //удаляем объект
+            //        db.Users.Remove(user);
+            //        db.SaveChanges();
+            //        // analog:
+            //        /*
+            //         db.Users.RemoveRange(user1, user2);
+            //    db.SaveChanges();
+            //         */
+            //    }
+            //    // выводим данные после обновления
+            //    Console.WriteLine("\nДанные после удаления:");
+            //    var users = db.Users.ToList();
+            //    foreach (User u in users)
+            //    {
+            //        Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+            //    }
+            //}
             Console.Read();
         }
     }
--- CoreConsoleEF/User.cs
+++ CoreConsoleEF/User.cs
@@ -9,5 +9,25 @@ namespace CoreConsoleEF
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
+
+        public IList<HomeUser> HomeUsers { get; set; }
     }
+
+    public class Home
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public IList<HomeUser> HomeUsers { get; set; }
+    }
+
+    public class HomeUser
+    {
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int HomeId { get; set; }
+        public Home Home { get; set; }
+    }
+
 }
